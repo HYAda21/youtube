33241fd690a89eb44bb67597bee33318474b5b81
Hemant <hyada2100@gmail.com>
2025-07-12 09:18:25 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,38 @@
+//euire('dotenv').config({path: './env'});
+
+//import mongoose from "mongoose"
+//import {DB_NAME} from "./constants"
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+
+  path:'./env'
+})
+
+connectDB();
+
+   /* as it is a second method as in that we not create another file like create in db as Index.js as write all the content here
+    await  mongoose.connect(`$(process.env.MONGODB_URL)/${DB_NAME}`)
+      app.on("error",(error)=>{
+        console.log("ERROR:",error);
+        throw error
+
+      })
+    app.listen(process.env.PORT,()=>{
+        console.log(`app is listening on port
+             ${process.env.PORT}`);
+    })
+
+    } catch (error) {
+        console.error("ERROR:",error)
+    
+        throw err
+        
+    }
+})()*/
+//important for databse alway learn
+// as in database learn a statement that database in othere contienent
+//  so we user async await and try catch in database to connnect 
+// its give very much error so we use try catch
